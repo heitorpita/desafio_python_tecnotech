f12ad1a12a442bb90554c64d2b76958b65c4c710
heitorpita <heitorpita96@gmail.com>
2025-12-01 22:34:23 -0300

    first commit

--- students/views.py
+++ students/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
 
-# Create your views here.
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/list.html', { 'students': students })
+
+def student_create(request):
+    if request.method == 'POST':
+        Student.objects.create(
+            nome=request.POST['nome'],
+            email=request.POST['email'],
+            cpf=request.POST['cpf'],
+            data_ingresso=request.POST['data_ingresso']
+        )
+        return redirect('student_list')
+
+    return render(request, 'students/create.html')
